c4a9cec067845f276f9edec34d6f93a7fe14a402
Adrain Lee <lxdiyun@gmail.com>
2013-05-22 12:50:47 +0800

    fix sortcase boundary error

--- sortcase/sortcase.py
+++ sortcase/sortcase.py
@@ -4,22 +4,21 @@ from re import sub
 
 
 def sort_books(case_no, start_index, end_index):
+#    print(case_no, start_index, end_index)
     # clean up old books
     books = Bookinfo.objects.all()
-    books = books.filter(bforcesortcase = 0)
-    books = books.filter(szbookcaseno = case_no)
-#    print('before: %d' % len(books))
-    books.update(szbookcaseno = '')
-
+    books = books.filter(bforcesortcase=0)
+    books = books.filter(szbookcaseno=case_no)
+    #    print('before: %d' % len(books))
+    books.update(szbookcaseno='')
 
     # update new books
     books = Bookinfo.objects.all()
-    books = books.filter(szpretendindexnum__gte = start_index)
-    books = books.filter(szpretendindexnum__lt = end_index)
-    books = books.filter(bforcesortcase = 0)
-#    print('after: %d' % len(books))
-    books.update(szbookcaseno = case_no)
-
+    books = books.filter(szpretendindexnum__gte=start_index)
+    books = books.filter(szpretendindexnum__lt=end_index)
+    books = books.filter(bforcesortcase=0)
+    #    print('after: %d' % len(books))
+    books.update(szbookcaseno=case_no)
 
 
 def sort_cases(cases):
@@ -28,31 +27,32 @@ def sort_cases(cases):
     pre_case = None
     for case in cases:
         print("[%d/%d] %s %s %s" % (count,
-                                 total,
-                                 case.szbookcaseno,
-                                 case.szfirstbookid ,
-                                 case.szpretendindexnum))
+                                    total,
+                                    case.szbookcaseno,
+                                    case.szfirstbookid,
+                                    case.szpretendindexnum))
         if 1 < count:
-            
             start_index = pre_case.szpretendindexnum
             end_index = case.szpretendindexnum
             start_prefix = sub(r'^(\d*[a-zA-Z]+).*$', r'\g<1>', start_index)
             end_prefix = sub(r'^(\d*[a-zA-Z]+).*$', r'\g<1>', end_index)
-            if start_prefix != end_prefix:
-                print('Error index %s' % case.szbookcaseno)
-                end_index = start_prefix + "{{{"
-            sort_books(pre_case.szbookcaseno, start_index, end_index)
 
+            if start_prefix != end_prefix:
+                print('Warning catalog change at %s ------------------'
+                      % case.szbookcaseno)
+
+            sort_books(pre_case.szbookcaseno, start_index, end_index)
 
         if count == total:
             start_index = case.szpretendindexnum
             end_index = sub(r'^(\d*[a-zA-Z]+).*$',
-                           r'\g<1>{{{',
-                           case.szpretendindexnum)
+                            r'\g<1>\\x453{{{',
+                            case.szpretendindexnum)
             sort_books(case.szbookcaseno, start_index, end_index)
 
         count += 1
         pre_case = case
+
 
 def sort_area(area_prefix):
     area = "%06d" % area_prefix
@@ -62,6 +62,7 @@ def sort_area(area_prefix):
     cases = cases.order_by('szpretendindexnum')
 
     sort_cases(cases)
+
 
 def sort_catalog(catalog_prefix):
     cases = Bookcaseidinfo.objects.all()
