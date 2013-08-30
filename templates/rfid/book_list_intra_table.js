5c7cbeca1f0beaaa9644b5fb6fcacbc54546d410
Adrain Lee <lxdiyun@gmail.com>
2013-08-30 11:18:53 +0800

    add datatable css/js plugin for checkbook

---
+++ templates/rfid/book_list_intra_table.js
@@ -0,0 +1,10 @@
+/* Table initialisation */
+$(document).ready(function() {
+	$('#book_list_table').dataTable( {
+		"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+		"sPaginationType": "bootstrap",
+		"oLanguage": {
+			"sLengthMenu": "_MENU_ records per page"
+		}
+	} );
+} );
